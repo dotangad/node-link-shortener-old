93a16c076c363eefa97ecf26045f56e1814ee5a2
Angad SIngh <angadsingh567890@gmail.com>
2018-05-27 03:03:47 +0530

    Backend done

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
-const app = require('express')();
+const bodyParser = require('body-parser');
+const express = require('express');
+const FileSync = require('lowdb/adapters/FileSync');
+const low = require('lowdb');
 
-app.get('/api/create_route', (req, res) => {
-  res.json(req.query);
+const app = express();
+
+const adapter = new FileSync('routes.json');
+const db = low(adapter);
+
+app.use(bodyParser.json({ inflate: true }));
+
+db.defaults({})
+  .write();
+
+function createRoute(req, res) {
+  const short = req.query.short || req.body.short;
+  // Use encodeURIComponent in the frontend to encode URL
+  const long = decodeURIComponent(req.query.long) || req.body.long;
+  if(db.has(short).value()) {
+    res.send('exists');
+  } else {
+    db.set(short, long)
+      .write();
+    res.send('success');
+  }
+  res.end();  
+}
+
+app.get('/api/create_route', createRoute);
+app.post('/api/create_route', createRoute);
+
+app.get('/api/get_routes', (req, res) => {
+  res.json(db.getState());
+});
+
+app.get('/:short', (req, res) => {
+  res.redirect(db.has(req.params.short).value());
+});
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.end('404');
 });
 
 app.listen(3000);
